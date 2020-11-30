d435f22a7894dea53e664ca61b1edf04f3d1a71d
Kai Daniels <dokikai@uw.edu>
2020-11-29 21:25:17 -0800

    Kai's sub w comments

--- analysis_summary_table.R
+++ analysis_summary_table.R
@@ -4,6 +4,8 @@ library("dplyr")
 # Reads CSV file of League champions into a Dataframe
 champions_df <- read.csv("data/LoL-Champions.csv", stringsAsFactors = FALSE)
 
+# Groups by champion class and then finds the average damage, tankiness,
+# mobility, and difficulty of the corresponding classes
 champ_table <- champions_df %>%
   group_by(Class) %>%
   summarize("Avg Damage" = mean(unlist(Damage)), 
