a281e1770ddfa0cc1a2400b3f9ff491e15b0d1ca
apple <apple@appledeMacBook-Air-2.local>
2020-11-30 23:25:29 -0800

    wenyi: change the code as a function

--- analysis_summary_table.R
+++ analysis_summary_table.R
@@ -1,14 +1,20 @@
 # Load in libraries
 library("dplyr")
-
+library("styler")
+library("lintr")
 # Reads CSV file of League champions into a Dataframe
 champions_df <- read.csv("data/LoL-Champions.csv", stringsAsFactors = FALSE)
 
 # Groups by champion class and then finds the average damage, tankiness,
 # mobility, and difficulty of the corresponding classes
-champ_table <- champions_df %>%
-  group_by(Class) %>%
-  summarize("Avg Damage" = mean(unlist(Damage)),
-            "Avg Tankiness" = mean(unlist(Sturdiness)),
-            "Avg Mobility" = mean(unlist(Mobility)),
-            "Avg Difficulty" = mean(unlist(Difficulty)))
+
+summary_table <- function(df){
+  plot <- df%>%
+       group_by(Class) %>%
+       summarize("Avg Damage" = mean(unlist(Damage)),
+                 "Avg Tankiness" = mean(unlist(Sturdiness)),
+                  "Avg Mobility" = mean(unlist(Mobility)),
+                  "Avg Difficulty" = mean(unlist(Difficulty)))
+  return(plot)
+}
+champ_table <- summary_table(champions_df)
