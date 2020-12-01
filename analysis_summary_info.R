be6c37a45ce3cc56695cc50e7f7fde9b51652fa9
Kai Daniels <dokikai@uw.edu>
2020-11-30 23:59:38 -0800

    final

--- analysis_summary_info.R
+++ analysis_summary_info.R
@@ -10,34 +10,26 @@ library("dplyr")
 #load table consisting of mean values of the different parameters(damage,tankiness,mobility,difficulty)
 source("analysis_summary_table.R")
 
-#calculates Class with highest mean damage
-highest_damage<- champ_table%>%
-  filter(`Avg Damage` == max(`Avg Damage`))%>%
-  pull(Class)
 
-#calculates Class with highest mean tankiness
-highest_tankiness<- champ_table%>%
-  filter(`Avg Tankiness` == max(`Avg Tankiness`))%>%
-  pull(Class)
-
-#calculates Class with highest mean mobility
-highest_mobility<- champ_table%>%
-  filter(`Avg Mobility` == max(`Avg Mobility`))%>%
-  pull(Class)
-
-#calculates Class with highest mean difficulty
-highest_difficulty<- champ_table%>%
-  filter(`Avg Difficulty` == max(`Avg Difficulty`))%>%
-  pull(Class)
-
-#calculates Class with lowest mean difficulty
-lowest_difficulty<- champ_table%>%
-  filter(`Avg Difficulty` == min(`Avg Difficulty`))%>%
-  pull(Class)
-
-             
-
+summary_function <- function(df) {
+  highest_damage <- df %>%
+    filter(`Avg Damage` == max(`Avg Damage`)) %>%
+    pull(Class)
+  highest_tankiness <- df%>%
+    filter(`Avg Tankiness` == max(`Avg Tankiness`))%>%
+    pull(Class)
+  highest_mobility<- df%>%
+    filter(`Avg Mobility` == max(`Avg Mobility`))%>%
+    pull(Class)
+  highest_difficulty<- df%>%
+    filter(`Avg Difficulty` == max(`Avg Difficulty`))%>%
+    pull(Class)
+  lowest_difficulty<- df%>%
+    filter(`Avg Difficulty` == min(`Avg Difficulty`))%>%
+    pull(Class)
+  result <- list(highest_damage, highest_tankiness, highest_mobility,
+                 highest_difficulty, lowest_difficulty)
+  return(result)
   
-  
-  
-  
+}
+summary <- summary_function(champ_table)
