b391c919b519af5e8b2855b615b9720efebb71b7
apple <apple@appledeMacBook-Air-2.local>
2020-12-08 03:55:55 -0800

    wenyi finish adding chart2

--- app_server.R
+++ app_server.R
@@ -2,7 +2,57 @@ library("shiny")
 library("ggplot2")
 library("dplyr")
 library("plotly")
+# Import  wenyi's data
+df_wards <- read.csv("data/2019-summer-match-data-OraclesElixir-2019-11-10.csv",
+                     stringsAsFactors = FALSE, fileEncoding = "UTF-8-BOM")
+# Wenyi: Define variable that wll be used in ui file
+max_death <- max(df_wards$teamdeaths)
+max_ward <- max(df_wards$wards)
+
 
 server <- function(input, output, session) {
+    output$message <- renderText({
+      if(input$var_team == 1){
+        msg <- paste0("You've chosen Win Team with wards interval [0,", 
+                      input$num_ward,"], and death inteval [0,", input$num_death,"]")
+      }
+      if(input$var_team== 0){
+        msg <- paste0("You've chosen Lose Team with wards interval [0,", 
+                      input$num_ward," ], and death inteval [0,", input$num_death,"]")
+      }
+      if(input$var_team== 2){
+        msg <- paste0("You've chosen Both Teams with wards interval [0,", 
+                      input$num_ward,"], and death inteval [0,", input$num_death,"]")
+      }
+      return(msg)
+    })
+    output$wpm_death_plot <- renderPlotly({
+      data <- df_wards %>%
+        group_by(gameid) %>%
+        filter(position == "Team") # select data that is useful to our analyze
+      # put the wards as x-axis, and number of death as y-axis, them put team name
+      # on the chats. Finally, we separate the win teams and fail team to make a
+      # clear comparison
+      
+      if(input$var_team == 1){
+        data <- filter(data, result == 0)
+      }
+      if(input$var_team== 0){
+        data <- filter(data, result == 1)
+      }
+      useful_data <-data%>%
+        ungroup()%>%
+        filter(wards <= input$num_ward, teamdeaths <= input$num_death)%>%
+        select(team, wards, teamdeaths)
+      
+      map <- ggplot(useful_data, mapping = aes(
+        x = wards, y = teamdeaths, color = team,
+      )) +
+        geom_point(stat = "identity") 
+      
+      map <- ggplotly(map)
+      return(map)
+    })
+  }
   
-}
+  
--- app_ui.R
+++ app_ui.R
@@ -1,3 +1,42 @@
+library("ggplot2")
+source("app_server.R")
+# Wenyi's start:
+wpm_sidebar_content <- sidebarPanel(
+  selectInput(
+    inputId = "var_team",
+    label = "Selct Teams",
+    choices = list("Win Teams" = "1", "Lose Teams" = "0", "Both" = "2"),
+    selected = "Both"
+  ),
+  sliderInput(
+    inputId = "num_ward",
+    label = "Choose a interval for the number of wards",
+    max = `max_ward`,
+    min = 0,
+    value = 0
+  ),
+  sliderInput(
+    inputId = "num_death",
+    label = "Choose a interval for the number of death",
+    max = `max_death`,
+    min = 0,
+    value = 0,
+    round = FALSE
+  )
+)
+
+wpm_main_content <- mainPanel(
+  h3("Scatter Plot in Terms of Wards and Death"),
+  textOutput(
+    "message"
+  ),
+  plotlyOutput(
+    "wpm_death_plot"
+  )
+)
+
+# Wenyi's end
+
 page_one <- tabPanel(
   "Page One (Overview)",
   h2("Put a caption here"),
@@ -17,13 +56,13 @@ page_two <- tabPanel(
 )
 
 page_three <- tabPanel(
-  "Page Title (Vis 2)",
-  h2("Put a caption here"),
-  sidebarLayout(
-    sidebarPanel(),
-    mainPanel()
+  "Wards VS Death",
+    sidebarLayout(
+      wpm_sidebar_content,
+      wpm_main_content
+    )
   )
-)
+
 
 page_four <- tabPanel(
   "Page Title (Vis 3)",
