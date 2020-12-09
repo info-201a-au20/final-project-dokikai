58132be2d2f22b28c933b4d0fccd662c30668285
michaelgov <michaelcgov@gmail.com>
2020-12-09 14:54:33 -0800

    Added styling and started page 1

--- app_ui.R
+++ app_ui.R
@@ -193,42 +193,53 @@ page_four <- tabPanel(
 )
 # Michael's End
 
+overview_image <- mainPanel(
+  HTML('<div id = "img">
+  <img src = "league.jpg", height = 493, width = 380>
+  </div>')
+)
+
 # Generates the overview for the project
 page_one <- tabPanel(
   "Project Overview",
-  h2("League of Legends - a Data Driven Project "),
+  h2("League of Legends - a Data Driven Project"),
   sidebarLayout(
     sidebarPanel(
       p("With Multiplayer Online Battle Arena (MOBA) games beinig some of the
         most popular video games in the world, its no doubt that League of
         Legends has had an immense cultural impact as the biggest MOBA in the
         world! But much like many other video games, League of Legends has many
-        variables and factors that can contribute to a teams chance of winning.
-        In this project, we wanted to answer three primary questions: (1) to
-        what extent winning certain objectives correlated with winninig the
-        match, (2) how professional League of Legends E-sports atheletes utilize
-        warding and vision mechanics and the gameplay effects that has, and (3)
-        the variability in banning certain champions in professional play along
-        with general champion statistics.")
+        variables and factors that can contribute to a teams chance of winning."),
+      p("In this project, we wanted to answer three primary questions:"), 
+      HTML("<ul><li>
+           To what extent would winning certain objectives 
+            correlate with winning the match?
+           </li>
+           <li>How do professional League of Legends E-sports atheletes utilize 
+           warding and vision mechanics and how do they effect gameplay?
+            </li>
+           <li>Does the variability in banning certain champions in 
+           professional play along with general champion statistics?
+            </li></ul>")
     ),
-    mainPanel()
+    overview_image
   )
 )
 
 page_five <- tabPanel(
   "Summary Takeaway",
   h2("Champion Statistics"),
-  p("Notable data insight or pattern – One notable data insight that we noticed
+  p("Notable data insight or pattern One notable data insight that we noticed
     in the visualization is that different classes of champions have different
     distributions of statistics like Style, Difficulty, Tankiness, Damage, etc.
-    This is shown specifically from the box plot visualization in the “Champion
-    Class, Skills, and Objectives” tab of the interface which has been
+    This is shown specifically from the box plot visualization in the Champion
+    Class, Skills, and Objectives tab of the interface which has been
     consolidated into the figure below."),
   p("[Insert Figure]"),
   p("From this table, some general observations we can make are that, on
     average, the Tank class is the easiest to play while Mage-Assasins, on
     average, are the most difficult. Furthermore, certain classes in general
-    have higher damage outputs – Assasins, Mage-Marksmans, and Marksman to name
+    have higher damage outputs Assasins, Mage-Marksmans, and Marksman to name
     a few."),
   p("These insights, furthermore, have broader implications for the game as a
     whole and contain valuable information for players across a broad spectrum
@@ -248,8 +259,8 @@ page_five <- tabPanel(
     and can help them better place battle strategy. A takeaway we have from our
     visualization is that there appeared to be a higher concentration of wards,
     on average, for the winning teams with generally less deaths on their parts
-    – so we can assess that there is at least some degree of positive
-    correlation between warding and winning in professional play. "),
+    so we can assess that there is at least some degree of positive
+    correlation between warding and winning in professional play."),
   p("This is, of course, specific to professional play but can provide insights
     into the game for players of all levels. And oftentimes, because warding is
     not prioritized as heavily in lower ELOs of play, newer or less experienced
@@ -259,13 +270,13 @@ page_five <- tabPanel(
     problem that newer players face with playing League. Oftentimes, people
     focus too much on getting gold (the currency in League) and kills but not
     enough on obtaining objectives like Dragon, Towers, or Inhibitors which
-    results in games that drag out and winning teams “throwing” their lead. In
+    results in games that drag out and winning teams throwing their lead. In
     fact, this emphasis on objective control is highlighted well in the Pie
-    Chart visualizations from the “Champion Class, Skills, and Objectives”
+    Chart visualizations from the Champion Class, Skills, and Objectives
     panel that show a very strong correlation between obtaining the first
     objectives and winning the game. This is most notably shown with the First
     Inhibitor visualization where 80% of teams that won the League match got the
-    first inhibitor – making a strong argument that good objective control and
+    first inhibitor making a strong argument that good objective control and
     map control tends to align with the winning team. In all, both in terms of
     warding and objective control, managing map presence appears the be a
     fundamental key to winning in League of Legends."),
